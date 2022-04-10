90fc4b5b4c42162abb4878865ee05fceb16b9005
ArviS <69751083+ArviSlayer@users.noreply.github.com>
2022-04-10 18:06:12 +0300

    Update yardim.js

--- yardim.js
+++ yardim.js
@@ -20,10 +20,18 @@ module.exports = {
   `
      **Lusia Komut Listesi**:
     `)
-  .addField('Eval', 'Admin', true)
-  .addField('Kayıt', 'Admin', true)
-  .addField('Ses Teyit', 'Admin', true)
-  .addField('Teyit', 'Admin', true)
+  .addFields([
+      }
+       name: 'Eval', 
+       value: 'Admin', 
+       inline: true
+      },
+      {      
+       name: 'Kayıt', 
+       value: 'Admin',
+       inline: true)
+      }
+    ])
   .setImage('https://tolgchu.notion.site/image/https%3A%2F%2Fcdn.discordapp.com%2Ficons%2F944714138624618497%2Fa_76fed393b04db76bbf6b3255ffbdadb5.gif%3Fsize%3D1024?table=block&id=ba3c1e49-ad8e-4f39-a830-e8f48c4b77b9&spaceId=77bed4b8-a251-4cf2-9e60-81bb8c733d67&userId=&cache=v2')
   .setTimestamp()
   message.channel.send({ embeds: [ypm] })
