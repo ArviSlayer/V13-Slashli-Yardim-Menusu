4713e1b2c66d14b4db1914d72570ccae919daca1
ArviS <69751083+ArviSlayer@users.noreply.github.com>
2022-04-10 17:36:44 +0300

    Create yardim.js

---
+++ yardim.js
@@ -0,0 +1,31 @@
+# v13-slash-yardim
+ArviS - V13 Slash Yardım Komutu
+const { ApplicationCommandType, ApplicationCommandOptionType, ChatInputCommandInteraction, Client } = require('discord.js');
+
+module.exports = {
+    type: ApplicationCommandType.ChatInput,
+    name: 'yardım',
+    description: 'Yardım Menüsü',
+    options: [],
+    /**
+     * Run a chat input command interaction.
+     * @param {ChatInputCommandInteraction} interaction The interaction.
+     * @param {Client} client The client of the bot.
+     */
+    async run(interaction, client) {
+        const ypm = new Discord.MessageEmbed()
+  .setTitle('Lusia Yardım Menüsü')
+  .setColor('#8387DE')
+  .setDescription(
+  `
+     **Lusia Komut Listesi**:
+    `)
+  .addField('Eval', 'Admin', true)
+  .addField('Kayıt', 'Admin', true)
+  .addField('Ses Teyit', 'Admin', true)
+  .addField('Teyit', 'Admin', true)
+  .setImage('https://tolgchu.notion.site/image/https%3A%2F%2Fcdn.discordapp.com%2Ficons%2F944714138624618497%2Fa_76fed393b04db76bbf6b3255ffbdadb5.gif%3Fsize%3D1024?table=block&id=ba3c1e49-ad8e-4f39-a830-e8f48c4b77b9&spaceId=77bed4b8-a251-4cf2-9e60-81bb8c733d67&userId=&cache=v2')
+  .setTimestamp()
+  message.channel.send({ embeds: [ypm] })
+    }
+};
